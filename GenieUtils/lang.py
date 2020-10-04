670d7cf54314d068040b3aba3047b9aa3738fa45
Kevin Armenat <armenat@medienpark.net>
2020-10-04 14:37:48 +0200

    simplified TXT lang loading

--- GenieUtils/lang.py
+++ GenieUtils/lang.py
@@ -1,16 +1,18 @@
 import csv
+from typing import Dict
 
 
-class Txt:
-    def __init__(self, path: str):
-        self.strings = dict()
+def load_txt(path: str) -> Dict[int, str]:
+    strings = dict()
 
-        with open(path) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=" ", quotechar='"')
+    with open(path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=" ", quotechar='"')
 
-            for row in csv_reader:
-                if len(row) == 2:
-                    try:
-                        self.strings[int(row[0])] = row[1]
-                    except ValueError:
-                        pass
+        for row in csv_reader:
+            if len(row) == 2:
+                try:
+                    strings[int(row[0])] = row[1]
+                except ValueError:
+                    pass
+
+    return strings
